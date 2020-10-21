89cedb3e961da26d2d09e12d1d725fe16a7a0ee8
repl.it user <replituser@example.com>
2020-10-21 21:22:50 +0000

    proces

---
+++ src/exercise4/ArrayInput.java
@@ -0,0 +1,22 @@
+package exercise4;
+
+import Java.util.Arrays;
+import Java.util.Scanner;
+
+public class ArrayInput {
+  public static void main (String[] args) {
+    
+  Scanner input = new Scanner(System.in);
+
+   int chislo = input.nextInt();
+   int[] arra0y = new int[chislo];
+   
+   for (int d = 0; d < array.length; d++){
+     array[d]=input.nextInt;
+   }
+
+  String numbersAsString = Arrays.toString(array);
+   System.out.println("Numbers: " + arrayAsString);
+
+}
+}
---
+++ src/exercise4/ArrayOutput.java
@@ -0,0 +1,18 @@
+package exercise4;
+
+import Java.util.Scanner;
+import Java.util.Arrays;
+
+public class ArrayOutput {
+  public static void main (String[] args){
+
+    Scanner input = new Scaner(System.in);
+        int[] masiv = new int [5];
+    System.out.println("Enter numbers: ");
+    String space = " ";
+    for (int index = 0; index < masiv.length; index++){
+      numbers[index] = input.nextInt();
+    }
+
+  }
+}
